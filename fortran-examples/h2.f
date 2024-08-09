6b8691bf06881d9734f164c6a118e7bb5ecc6d64
Christian Ortiz <68184749+statsconchris@users.noreply.github.com>
2024-08-09 03:03:44 +0200

    Update h2.f

--- fortran-examples/h2.f
+++ fortran-examples/h2.f
@@ -3,7 +3,7 @@
       include 'mpif.h'
       PARAMETER( NDIM = 10 )
       DIMENSION A(NDIM)
-C
+
       call MPI_INIT( ierr )
       call MPI_COMM_RANK( MPI_COMM_WORLD, myid, ierr )
       call MPI_COMM_SIZE( MPI_COMM_WORLD, numprocs, ierr )
@@ -11,23 +11,23 @@ C
       MM=NDIM/numprocs
       IMIN=1+MM*myid
       IMAX=MM*(myid+1)
-C
+
       DO I=IMIN,IMAX
       A(I)=I
       ENDDO
-C
+
       IF (myid.EQ.0) THEN
       DO I=1,NDIM
       WRITE(6,*) I,A(I)
       ENDDO
       ENDIF
-C
+
       IF (myid.EQ.1) THEN
       DO I=1,NDIM
       WRITE(6,*) I,A(I)
       ENDDO
       ENDIF
-C
+
       call MPI_FINALIZE(ierr)
       STOP
       END
